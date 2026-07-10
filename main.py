2e6a19bf26561d2535ee0a7e9f7524dc5035206e
islombaxtiyorov063@gmail.com <islombaxtiyorov063@gmail.com>
2026-07-10 06:01:50 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+a = float(input("Birinchi sonni kiriting:"))
+b = float(input("Ikkinchi sonni kiriting:"))
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
