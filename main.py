4e54b4b1731dc862107f14d80a17517a94713736
islombaxtiyorov063@gmail.com <islombaxtiyorov063@gmail.com>
2026-07-10 06:52:24 +0500

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-a = float(input("Birinchi sonni kiriting:"))
-b = float(input("Ikkinchi sonni kiriting:"))
+# a = float(input("Birinchi sonni kiriting:"))
+# # b = float(input("Ikkinchi sonni kiriting:"))
+# #
+# # print(f"{a} + {b} = {a + b}")
+# # print(f"{a} - {b} = {a - b}")
+# # print(f"{a} * {b} = {a * b}")
+# # print(f"{a} / {b} = {a / b}")
 
-print(f"{a} + {b} = {a + b}")
-print(f"{a} - {b} = {a - b}")
-print(f"{a} * {b} = {a * b}")
-print(f"{a} / {b} = {a / b}")
+
+
+ismlar = ["Abror", "Mahmud", "Sardor"]
+
+print("Salom " + ismlar[0] + ", bugun choyxona bormi?")
+print(ismlar[1] + ", choyxonaga boramizmi?")
+print("Salom " + ismlar[2] + ", qalaysan?")
+
+sonlar = [10, -5, 25, 4.5]
+
+print(sonlar[0] + sonlar[1])
+print(sonlar[2] * 2)
+
+sonlar[0] = 50
+sonlar[1] = -20
+
+print(sonlar)
